e92246bf1d283003eebe1a9efc9470f4a7b88b9d
Ubrufih Orevaoghene <102390650+Kreeeva@users.noreply.github.com>
2023-10-30 20:31:44 +0100

    corrections final.py

--- final.py
+++ final.py
@@ -67,7 +67,7 @@ def update_input_container(selected_statistics):
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
     Output(component_id='output-container', component_property='children'),
-    [Input(component_id='select-year', component_property='value'), Input(component_id='dropdown-statistics', component_property='value')])
+    [Input(component_id='dropdown-statistics', component_property='value'), Input(component_id='select-year', component_property='value')])
 
 
 def update_output_container(selected_statistics, input_year ):
@@ -100,7 +100,7 @@ def update_output_container(selected_statistics, input_year ):
         )
 
 # Plot 4 bar chart for the effect of unemployment rate on vehicle type and sales
-        unemp =recession_data.groupby('Vehicle_Type')['Automobile_Sales','unemployment_rate'].mean().reset_index()
+        unemp =recession_data.groupby('Vehicle_Type')[['Automobile_Sales','unemployment_rate']].mean().reset_index()
         R_chart4 = dcc.Graph(figure=px.bar(unemp, x='Vehicle_Type', y='Automobile_sales', title='effect of unemployment')
         
         return [
@@ -124,7 +124,7 @@ def update_output_container(selected_statistics, input_year ):
 
             # Plot bar chart for average number of vehicles sold during the given year
         avr_vdata=yearly_data.groupby('Month')['Automobile_Sales'].mean().reset_index()
-        Y_chart3 = dcc.Graph( figure=px.bar,x='Month',y='Automobile_Sales',title='Average Vehicles Sold by Vehicle Type in the year {}'.format(input_year)))
+        Y_chart3 = dcc.Graph( figure=px.bar(x='Month',y='Automobile_Sales',title='Average Vehicles Sold by Vehicle Type in the year {}'.format(input_year)))
 
             # Total Advertisement Expenditure for each vehicle using pie chart
         exp_data=yearly_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
